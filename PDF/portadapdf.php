c29f88eda1f2a9dc002ae53f0eb822cd37145908
pabloarielbocchio <pablobocchio@gmail.com>
2024-07-03 08:04:41 -0300

    pto 1

--- PDF/portadapdf.php
+++ PDF/portadapdf.php
@@ -325,7 +325,7 @@ if (isset($_GET["codigo"])) {
                 <table cellspacing="0" style="width: 100%; text-align: left; font-size: 10pt;">
                     <tr>
                         <th class="filatituloOt">
-                            OT:
+                            OV#/PY:
                         </th>
                         <th class="filatextoOt"><?php echo $codigo_cliente; ?></th>
 
